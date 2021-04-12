3c3c569d1aa63611cf43639d16f8b21e927d82bf
miguelcoria94 <miguelcoria94@gmail.com>
2021-04-11 19:17:21 -0500

    added nimiq network

---
+++ script.js
@@ -0,0 +1,12 @@
+Nimiq.load().then(async function () {
+
+    // Configure for Testnet. For Mainnet, use main().
+    Nimiq.GenesisConfig.test();
+
+    // Instantiate a config builder.
+    const configBuilder = Nimiq.Client.Configuration.builder();
+
+    // Create a client based on your configuration,
+    // it will automatically connect to the network.
+    const client = configBuilder.instantiateClient();
+});
